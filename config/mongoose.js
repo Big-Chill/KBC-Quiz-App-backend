d9d5db50ac684030c72da0428497c6ab77f3f450
Rohit Nandy <rohitnandy39@gmail.com>
2023-02-05 18:59:45 +0530

    Update MongoDB URL

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', false); // To allow queries like: Model.find({ $or: [{ name: 'John' }, { name: 'Jane' }] }
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/Quiz_db', { useNewUrlParser: true })
+mongoose.connect('mongodb+srv://DevilsBreath39:abcdwxyz@quiz-db.ubvgn2x.mongodb.net/Quiz_db', { useNewUrlParser: true })
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
