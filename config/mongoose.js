0890794f43cf6198fe9acc562d54facc7fa0229c
Rohit Nandy <rohitnandy39@gmail.com>
2023-03-09 20:24:15 +0530

    Fixed env variable issue

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose');
+const path = require('path');
+const os = require('os');
 const dotenv = require('dotenv');
+
+if (os.platform() === 'win32') {
+  dotenv.config({ path: path.join(__dirname, `../.env.${process.env.NODE_ENV}`.trim()) });
+}
+
 const mongodb_url = process.env.MONGO_DB;
+
+
 mongoose.set('strictQuery', false); // To allow queries like: Model.find({ $or: [{ name: 'John' }, { name: 'Jane' }] }
 
 
