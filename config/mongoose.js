5118880d8894030ac3dc7746b0fe5e6ba07da072
Rohit Nandy <rohitnandy39@gmail.com>
2023-03-09 19:58:11 +0530

    Minor change

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
-const mongodb_url = process.env.MONGODB_URL;
+const mongodb_url = process.env.MONGO_DB;
 mongoose.set('strictQuery', false); // To allow queries like: Model.find({ $or: [{ name: 'John' }, { name: 'Jane' }] }
 
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://DevilsBreath39:abcdwxyz@quiz-db.ubvgn2x.mongodb.net/Quiz_db', { useNewUrlParser: true })
+mongoose.connect(mongodb_url, { useNewUrlParser: true })
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
