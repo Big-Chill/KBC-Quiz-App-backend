d479c847b4e5a170634d75bc75abf68569bdca68
Rohit Nandy <rohitnandy39@gmail.com>
2023-03-09 19:51:44 +0530

    Fixed env bug

--- config/mongoose.js
+++ config/mongoose.js
@@ -6,7 +6,7 @@ mongoose.set('strictQuery', false); // To allow queries like: Model.find({ $or: 
 
 
 // Connect to MongoDB
-mongoose.connect(mongodb_url, { useNewUrlParser: true })
+mongoose.connect('mongodb+srv://DevilsBreath39:abcdwxyz@quiz-db.ubvgn2x.mongodb.net/Quiz_db', { useNewUrlParser: true })
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
